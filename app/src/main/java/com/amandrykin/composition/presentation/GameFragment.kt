ed4601edac391278eb755c3926c5d6018ba1e375
amandrykin <amandrykin@yandex.ru>
2022-04-02 00:30:30 +0300

    ViewBinding

--- app/src/main/java/com/amandrykin/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/amandrykin/composition/presentation/GameFragment.kt
@@ -1,18 +1,28 @@
 package com.amandrykin.composition.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.amandrykin.composition.R
+import androidx.fragment.app.Fragment
+import com.amandrykin.composition.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
